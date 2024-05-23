960181ca47010e8f2d737387ba32a710b30f4cf2
yuu <46545607+mouseos@users.noreply.github.com>
2024-05-23 21:54:26 +0900

    landscape_hdpi

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -97,7 +97,7 @@ VENDOR_SECURITY_PATCH := 2099-12-31
 PLATFORM_VERSION := 16.1.0
 
 # TWRP Configuration
-TW_THEME := portrait_hdpi
+TW_THEME := landscape_hdpi
 TW_EXTRA_LANGUAGES := true
 TW_SCREEN_BLANK_ON_BOOT := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
