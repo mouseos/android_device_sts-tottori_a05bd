354a7606ecf5e870b1442552f897b8ab271d5caf
yuu <46545607+mouseos@users.noreply.github.com>
2024-05-23 23:49:27 +0900

    諦めて縦表示のUIにする

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -97,7 +97,7 @@ VENDOR_SECURITY_PATCH := 2099-12-31
 PLATFORM_VERSION := 16.1.0
 
 # TWRP Configuration
-TW_THEME := landscape_hdpi
+TW_THEME := portrait_hdpi
 TW_EXTRA_LANGUAGES := false
 TW_DEFAULT_LANGUAGE := ja-JP
 TW_SCREEN_BLANK_ON_BOOT := true
