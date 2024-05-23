a398b17343d834941dd0f8064d0195d7a3d1ee03
yuu <46545607+mouseos@users.noreply.github.com>
2024-05-23 21:45:49 +0900

    複数言語対応

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -98,7 +98,7 @@ PLATFORM_VERSION := 16.1.0
 
 # TWRP Configuration
 TW_THEME := portrait_hdpi
-TW_EXTRA_LANGUAGES := false
+TW_EXTRA_LANGUAGES := true
 TW_SCREEN_BLANK_ON_BOOT := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_USE_TOOLBOX := true
